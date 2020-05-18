71cd49ce40c40495d014ac951614b0531d377c5b
mhashim6 <m6hashim@gmail.com>
2020-05-18 03:51:49 +0200

    Updates

---
+++ precache-manifest.cbc88b447f8185650fd1b88fa504184f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "46940047c8ea7275e034",
+    "url": "/static/css/main.d42c888d.chunk.css"
+  },
+  {
+    "revision": "46940047c8ea7275e034",
+    "url": "/static/js/main.46940047.chunk.js"
+  },
+  {
+    "revision": "e4afadcd683732496215",
+    "url": "/static/js/1.e4afadcd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "0e3651c6b7bb14abc5b294c1f546db83",
+    "url": "/index.html"
+  }
+];
